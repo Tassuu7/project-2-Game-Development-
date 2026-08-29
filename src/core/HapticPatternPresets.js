54032f247ee97269f20b58e43318e70b92f920ca
Shaik Tasleema Sadiya <shaiktasleemasadiya7@gmail.com>
2026-08-29 17:42:36 +0530

    feat(input): add HAPTIC_PATTERNS vibration sequence presets

---
+++ src/core/HapticPatternPresets.js
@@ -0,0 +1,9 @@
+/**
+ * NovaForge Gamepad Haptic Vibration Patterns
+ * @author NovaForge Engineering Team
+ * @license Proprietary - All Rights Reserved
+ */
+export const HAPTIC_PATTERNS = {
+    HEARTBEAT: [{ duration: 100, strong: 0.8 }, { duration: 150, strong: 0.4 }],
+    CRITICAL_HIT: [{ duration: 250, strong: 1.0, weak: 0.9 }]
+};
